ad7d3eb6e491258d5fc5b95bd6fe8bc97e3a6ba1
TS22082 <ts22082@gmail.com>
2025-07-09 13:27:52 -0700

    set up authentication

--- src/routes/login/index.tsx
+++ src/routes/login/index.tsx
@@ -1,5 +1,25 @@
 import { component$ } from "@builder.io/qwik";
+import { Button, Jumbotron } from "flowbite-qwik";
+import { Form } from "@builder.io/qwik-city";
+import { IconUserOutline } from "flowbite-qwik-icons";
+import { useSignIn } from "../plugin@auth";
 
 export default component$(() => {
-  return <h1>Login page</h1>;
+  const signIn = useSignIn();
+  return (
+    <Jumbotron>
+      <Jumbotron.Heading tag="h2">
+        Sign In to Access Your Analytics Dashboard
+      </Jumbotron.Heading>
+      <Jumbotron.SubText>
+        Gain valuable insights and monitor your application’s performance with
+        Flowbite's advanced logging and analytics tools.
+      </Jumbotron.SubText>
+      <section class="mt-10 flex justify-center gap-10">
+        <Form action={signIn}>
+          <Button prefix={IconUserOutline}>Login / Register</Button>
+        </Form>
+      </section>
+    </Jumbotron>
+  );
 });
