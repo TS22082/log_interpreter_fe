bb1b8bb4c409045f8413c504a2363eb63e56fde7
TS22082 <ts22082@gmail.com>
2025-07-09 19:27:45 -0700

    setup client id and secret for auth

--- src/routes/login/index.tsx
+++ src/routes/login/index.tsx
@@ -1,33 +1,36 @@
-import { component$ } from "@builder.io/qwik";
+import { $, component$ } from "@builder.io/qwik";
 import { Button, Jumbotron } from "flowbite-qwik";
 import { IconGithubSolid } from "flowbite-qwik-icons";
 import { useSignIn } from "../plugin@auth";
 
 export default component$(() => {
   const signIn = useSignIn();
+
+  const loginWith = $((provider: string) => {
+    return signIn.submit({
+      providerId: provider,
+      redirectTo: "/",
+    });
+  });
+
   return (
     <Jumbotron>
       <Jumbotron.Heading tag="h2">
-        Access Your Analytics and Logs Dashboard
+        Access Your Services Information Dashboard
       </Jumbotron.Heading>
-      <Jumbotron.SubText>
-        Gain valuable insights and monitor your application’s performance with
-        Ai-kno's' advanced logging and analytics tools.
-      </Jumbotron.SubText>
       <Jumbotron.SubText>
         Choose a provider to either login or register
       </Jumbotron.SubText>
       <section class="mt-10 flex justify-center gap-10">
+        <Button onClick$={() => loginWith("github")} prefix={IconGithubSolid}>
+          Github
+        </Button>
         <Button
-          onClick$={() =>
-            signIn.submit({
-              providerId: "github",
-              redirectTo: "/",
-            })
-          }
+          disabled
+          onClick$={() => loginWith("google")}
           prefix={IconGithubSolid}
         >
-          Github
+          Google
         </Button>
       </section>
     </Jumbotron>
